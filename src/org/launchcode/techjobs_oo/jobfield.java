5ee4480383c8887766ddefec893736a3bb8e562e
aramic1989 <63340759+aramic1989@users.noreply.github.com>
2020-07-29 04:11:47 -0500

    Update jobfield.java

--- src/org/launchcode/techjobs_oo/jobfield.java
+++ src/org/launchcode/techjobs_oo/jobfield.java
@@ -1,47 +1,38 @@
 package org.launchcode.techjobs_oo;
-
 import java.util.Objects;
-
 public class JobField {
-    public int id;
+    public int id
     public static int nextId = 1;
     public String value;
 
     public JobField(){
         id = nextId;
-        nextId++;
+        nextId++
     }
-
     public JobField(String value){
         this();
         this.value = value;
     }
-
     public String toString(){
         return value;
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        JobField jobField = (JobField) o;
-        return id == jobField.id;
+        JobField jobField = (JobField) o
+        return id == jobField.id
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id)
     }
-
     public int getId() {
         return id;
     }
-
     public String getValue() {
         return value;
     }
-
     public void setValue(String value) {
         this.value = value;
     }
